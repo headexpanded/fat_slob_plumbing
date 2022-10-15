dc77c2b1e03a75129cb380efc8435e37ba12fa1a
moncon <monconish@protonmail.com>
2022-10-15 23:36:24 +0200

    Intro Section

--- components/sections/index.ts
+++ components/sections/index.ts
@@ -1,7 +1,11 @@
 // section components index for Fat Slob Plumbing
 
 import { ServicesSection } from "./ServicesSection";
+import { IntroSection } from "./IntroSection";
+import { CustReviewsSection } from "./CustReviewsSection";
 
 export const Sections = {
     ServicesSection,
+    IntroSection,
+    CustReviewsSection,
 }
