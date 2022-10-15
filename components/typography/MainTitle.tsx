5c43dc036caa198c2aaaaa3cc88da7a72f9c0c2b
moncon <monconish@protonmail.com>
2022-10-15 12:55:27 +0200

    title color, styling

--- components/typography/MainTitle.tsx
+++ components/typography/MainTitle.tsx
@@ -3,20 +3,22 @@
 //  define MainTitle type properties
 type MainTitleProps = {
     content: string;
-    color?: "#804D00";
+    color?: "#875e04";
 }
 
 // define MainTitle component
-export const MainTitle =({content, color="#804D00"}:MainTitleProps) =>{
+export const MainTitle =({content, color="#875e04"}:MainTitleProps) =>{
     return (
         <>
         <h1>{content}</h1>
         <style jsx>{`
             h1{
+                color: ${color??"#875e04"}
                 font-size: 4rem;
-                color: ${color??"#804D00"}
+                line-height: 1.15;
                 padding:12px;
                 margin-top:8px;
+                text-align: center;
             }
         `}</style>
         </>
