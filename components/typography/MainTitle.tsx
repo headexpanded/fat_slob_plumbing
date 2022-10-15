d84c0fd10e1ab0b7470edc681cdb8ee20ca13228
moncon <monconish@protonmail.com>
2022-10-15 13:03:53 +0200

    color goddammit

--- components/typography/MainTitle.tsx
+++ components/typography/MainTitle.tsx
@@ -1,19 +1,20 @@
 // define properties of Main Title
+// color is #804D00 until project color palette is finalised
 
 //  define MainTitle type properties
 type MainTitleProps = {
     content: string;
-    color?: "#875e04";
+    color?: "#804D00";
 }
 
 // define MainTitle component
-export const MainTitle =({content, color="#875e04"}:MainTitleProps) =>{
+export const MainTitle =({content, color="#804D00"}:MainTitleProps) =>{
     return (
         <>
         <h1>{content}</h1>
         <style jsx>{`
             h1{
-                color: ${color??"#875e04"}
+                color: ${color??"#804D00"}
                 font-size: 5rem;
                 line-height: 1.15;
                 padding:12px;
