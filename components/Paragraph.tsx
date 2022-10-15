8f0b34a931bb85b11a1d294cfbb7b2bf0ff0594a
moncon <monconish@protonmail.com>
2022-10-15 10:13:00 +0200

    Paragraph text component

---
+++ components/Paragraph.tsx
@@ -0,0 +1,23 @@
+// define properties of paragraph texts
+// color is white until project color palette is determined
+
+// define Paragraph type properties
+type ParagraphProps ={
+    content: string;
+    color?: "white"
+}
+
+// define Paragraph component
+export const Paragraph =({content, color="white"}:ParagraphProps) =>{
+    return (
+        <>
+        <p>{content}</p>
+        <style jsx>{`
+            p {
+                color: ${color??"white"};
+                font-size: 16px;
+            }
+        `}</style>
+        </>
+    )
+}
