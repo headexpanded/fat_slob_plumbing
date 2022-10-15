4024a19ce5da91bb3af1a9dcbefc08f8eff6e330
moncon <monconish@protonmail.com>
2022-10-15 13:01:38 +0200

    5rem

--- components/typography/MainTitle.tsx
+++ components/typography/MainTitle.tsx
@@ -14,7 +14,7 @@ export const MainTitle =({content, color="#875e04"}:MainTitleProps) =>{
         <style jsx>{`
             h1{
                 color: ${color??"#875e04"}
-                font-size: 4rem;
+                font-size: 5rem;
                 line-height: 1.15;
                 padding:12px;
                 margin-top:8px;
