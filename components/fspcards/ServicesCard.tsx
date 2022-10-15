4509ad7810792e7622a4e559e43ed0c653bc9b25
moncon <monconish@protonmail.com>
2022-10-16 00:50:13 +0200

    services prices

--- components/fspcards/ServicesCard.tsx
+++ components/fspcards/ServicesCard.tsx
@@ -12,14 +12,20 @@ export const ServicesCard =({title, content, price}:ServicesCardProps) =>{
     return (
         <>
         <div className="col-sm px-5 mt-1 mb-1 servicesCard">
-            <h2>{title}</h2><p>{content}</p>
-        </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h2>{title}</h2><p>{content}</p>
+                    </div>
+                </div>
+            </div>
         <div className="container d-flex flex-row text-center">
             <div className="row align-items-end justify-content-center">
                 <div className="col">
                     <Typography.SubHeader content ="only"/><Typography.SectionTitle content ={price}/>
                 </div>
             </div>
+        </div>
         </div>
         <style jsx>{`
             .servicesCard{
