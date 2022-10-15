cd995c122bd16f1b3b6acddeac057517f25d1b9b
moncon <monconish@protonmail.com>
2022-10-15 23:47:50 +0200

    Intro Section

--- components/sections/IntroSection.tsx
+++ components/sections/IntroSection.tsx
@@ -14,7 +14,7 @@ return (
         <div className="introSub"><Typography.SubHeader content="Stress testing domestic sewage systems since 2013"/></div>
         <div className="container px-2 pb-1">
             <div className="row gap-1">
-                <div className="col">
+                <div className="col-sm">
                     <Typography.Paragraph content="What's the capacity of YOUR home's sewage system?"/>
                     <Typography.Paragraph content = "Most people don't know. But you should! These days most homes just can't deal with non-standard loads."/>
                     <Typography.Paragraph content = "Take our advice - you need to stress test your system now. After all, no-one wants an unplanned blockage!"/>
