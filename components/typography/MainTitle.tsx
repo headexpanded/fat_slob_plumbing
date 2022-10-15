f2abcf5102161ba67e842b97faa5798421d66bd4
moncon <monconish@protonmail.com>
2022-10-15 11:08:19 +0200

    MainTitle component

---
+++ components/typography/MainTitle.tsx
@@ -0,0 +1,25 @@
+// define properties of Main Title
+// color is black until project color palette is determined
+
+//  define MainTitle type properties
+type MainTitleProps = {
+    content: string;
+    color?: "black";
+}
+
+// define MainTitle component
+export const MainTitle =({content, color="black"}:MainTitleProps) =>{
+    return (
+        <>
+        <h1>{content}</h1>
+        <style jsx>{`
+            h1{
+                font-size: 36px;
+                color: ${color??"black"}
+                padding:12px;
+                margin-top:12px;
+            }
+        `}</style>
+        </>
+    );
+};
