3fc9b8004827ab72943f8d5b3cfd1adfec30cbe6
moncon <monconish@protonmail.com>
2022-10-15 13:00:38 +0200

    font color

--- components/typography/Paragraph.tsx
+++ components/typography/Paragraph.tsx
@@ -1,20 +1,20 @@
 // define properties of paragraph texts
-// color is black until project color palette is determined
+// color is #804D00 until project color palette is finalised
 
 // define Paragraph type properties
 type ParagraphProps ={
     content: string;
-    color?: "black"
+    color?: "#804D00"
 }
 
 // define Paragraph component
-export const Paragraph =({content, color="black"}:ParagraphProps) =>{
+export const Paragraph =({content, color="#804D00"}:ParagraphProps) =>{
     return (
         <>
         <p>{content}</p>
         <style jsx>{`
             p {
-                color: ${color??"black"};
+                color: ${color??"#804D00"};
                 font-size: 16px;
                 padding: 8px;
             }
