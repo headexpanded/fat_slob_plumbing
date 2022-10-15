1ae4755e757cfe53c3ffe6c91992bfb5f02c08fa
moncon <monconish@protonmail.com>
2022-10-15 16:18:05 +0200

    text styles

--- components/cards/ServicesCard.tsx
+++ components/cards/ServicesCard.tsx
@@ -19,16 +19,21 @@ export const ServicesCard =({title, content}:ServicesCardProps) =>{
                 text-align:left;
                 transition: color 0.15s ease, border-color 0.15s ease;
             }
-            
-            .servicesCard h2 {
-                font-size: 1.5rem;
-                font-weight:bold;
+            .servicesCard h2, p {
+                color: #804d00;
                 padding: 6px;
                 margin: 0 0 1rem 0;
             }
+
+            .servicesCard h2 {
+                font-size: 1.5rem;
+                font-weight:bold;
+            }
+            
             .servicesCard p {
                 font-size: 1.25rem
-                margin: 0px;
+                margin: 0 0 1rem 0;
+                padding: 6px;
             }
         
         `}</style>
