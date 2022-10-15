e7f4817afc8461ba940fbdccd170ccfd6b1c2ac3
moncon <monconish@protonmail.com>
2022-10-15 13:53:35 +0200

    Italic

---
+++ components/typography/Italic.tsx
@@ -0,0 +1,21 @@
+// define properties of Italic texts
+
+// define Italic type properties
+type ItalicProps = {
+    content: string;
+    color?: "#804D00";
+}
+
+// define Italic component
+export const Italic =({content, color="#804D00"}:ItalicProps) => {
+    return(
+        <style jsx>{`
+        p {
+            color: ${color??"#804D00"};
+            font-size: 20px;
+            text-align: center;
+            font-style: italic;
+        }
+        `}</style>
+    )
+}
