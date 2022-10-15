9e7bee2b5080ea21c9292106b40067af8bc95381
moncon <monconish@protonmail.com>
2022-10-16 01:02:03 +0200

    services prices

--- components/fspcards/ServicesCard.tsx
+++ components/fspcards/ServicesCard.tsx
@@ -19,8 +19,8 @@ export const ServicesCard =({title, content, price}:ServicesCardProps) =>{
                     </div>
                 </div>
             </div>
-        <div className="container d-flex flex-row">
-            <div className="row align-items-end justify-content-center">
+        <div className="container d-flex flex-row align-items-end justify-content-center">
+            <div className="row ">
                 <div className="col pe-0">
                     <Typography.SubHeader content ="only"/>
                 </div>
