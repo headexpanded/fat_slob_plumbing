3475847ce0a60ed0f3a1f24561fdd4f4b56294f6
moncon <monconish@protonmail.com>
2022-10-15 17:53:05 +0200

    import bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
 import '../styles/globals.css'
+import { useEffect } from "react";
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
   return <Component {...pageProps} />
 }
 
