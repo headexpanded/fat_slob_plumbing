68319103215e98b9a0504088ebcb1d3540b7c8ce
moncon <monconish@protonmail.com>
2022-10-16 13:11:01 +0200

    var colors

--- components/typography/SubHeader.tsx
+++ components/typography/SubHeader.tsx
@@ -4,17 +4,17 @@
 // define Italic type properties
 type SubHeaderProps = {
     content: string;
-    color: "`{--lemonCurry}`";
+    color: "#be9b0c";
 }
 
 // define Italic component. Use for sub-title slogans, customer reviews
-export const SubHeader =({content, color="`{--lemonCurry}`"}:SubHeaderProps) => {
+export const SubHeader =({content, color="#be9b0c"}:SubHeaderProps) => {
     return(
         <>
         <h4>{content}</h4>
         <style jsx>{`
         h4 {
-            color: ${color??"`{var(--lemonCurry}`"};
+            color: ${color??"#be9b0c"};
             font-size: 20px;
             text-align: center;
             font-style: italic;
