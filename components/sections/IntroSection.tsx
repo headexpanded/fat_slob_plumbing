6f0e0bf86e46644c5213a6dc39a2229783566000
moncon <monconish@protonmail.com>
2022-10-16 13:38:30 +0200

    intro text container

--- components/sections/IntroSection.tsx
+++ components/sections/IntroSection.tsx
@@ -16,14 +16,14 @@ return (
             <div className="row gap-1">
                 <div className="col-sm">
                     <Typography.SectionTitle content="What's the capacity of YOUR home's sewage system?"/>
-                    <div className="container">
-                        <div className = "row">
-                            <div className = "col-3" /></div>
-                            <div className = "col-6"><Typography.Paragraph content = "A lot of people don't know. But you should! Most home systems can't flush a big load. Take our advice - you need to stress test your system, NOW! Why? Because no-one likes a blockage!"/></div>
-                            <div className = "col-3" /></div>
-                        </div>
-                    </div>
                 </div>
+            </div>
+        </div>
+        <div className="container">
+            <div className="row">
+                <div className="col-2"></div>
+                <div className="col-8"><Typography.Paragraph content = "A lot of people don't know. But you should! Most home systems can't flush a big load. Take our advice - you need to stress test your system, NOW! Why? Because no-one likes a blockage!"/></div>
+                <div className="col-2"></div>
             </div>
         </div>
     </section>
