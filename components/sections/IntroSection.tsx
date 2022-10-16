ab728e753053570315a61b14fe2c9a3ac9ade445
moncon <monconish@protonmail.com>
2022-10-16 13:07:30 +0200

    var colors

--- components/sections/IntroSection.tsx
+++ components/sections/IntroSection.tsx
@@ -17,9 +17,9 @@ return (
                 <div className="col-sm">
                     <Typography.SectionTitle content="What's the capacity of YOUR home's sewage system?"/>
                     <Typography.SectionTitle content = "Most people don't know. But you should!"/>
-                    <Typography.Paragraph content="These days most homes just can't deal with non-standard loads."/>
-                    <Typography.Paragraph content = "Take our advice - you need to stress test your system, NOW!"/>
-                    <Typography.Paragraph content = "After all, no-one wants an unplanned blockage!"/>
+                    <Typography.SectionTitle content="These days most homes just can't deal with non-standard loads."/>
+                    <Typography.SectionTitle content = "Take our advice - you need to stress test your system, NOW!"/>
+                    <Typography.SectionTitle content = "After all, no-one wants an unplanned blockage!"/>
                 </div>
             </div>
         </div>
