7778fe49b6e00b9df482d9e6eec1d36b090ad83f
moncon <monconish@protonmail.com>
2022-10-26 21:28:32 +0200

    NavBar, NavLink

--- components/sections/NavBarSection.tsx
+++ components/sections/NavBarSection.tsx
@@ -6,8 +6,24 @@ type NavBarSectionProps = {};
 export const NavBarSection = () => {
   return (
     <>
-      <section className="navBarSection"></section>
-      <style jsx>{``}</style>
+      <section className="navBarSection">
+        <div className="container d-flex flex-row px-5 justify-content-around">
+          <Typography.NavLink navLink="HOME" color="var(--solidBrown)" />
+          <Typography.NavLink navLink="SERVICES" color="var(--solidBrown)" />
+          <Typography.NavLink
+            navLink="RESERVATIONS"
+            color="var(--solidBrown)"
+          />
+          <Typography.NavLink navLink="FRANCHISES" color="var(--solidBrown)" />
+          <Typography.NavLink navLink="T-SHIRTS" color="var(--solidBrown)" />
+          <Typography.NavLink navLink="PARTNERS" color="var(--solidBrown)" />
+        </div>
+      </section>
+      <style jsx>{`
+        .navBarSection {
+          font-size: 0.5rem;
+        }
+      `}</style>
     </>
   );
 };
---
+++ components/typography/NavLink.tsx
@@ -0,0 +1,30 @@
+// define properties of NavLink texts
+
+// define NavBar type properties
+type NavLinkProps = {
+  navLink: string;
+  color?: string;
+};
+
+// define NavLink component. Use for navigation links
+export const NavLink = ({ navLink, color }: NavLinkProps) => {
+  return (
+    <>
+      <h4>{navLink}</h4>
+      <style jsx>{`
+        h4 {
+          color: ${color ?? ""};
+          font-size: 0.9rem;
+          padding: 20px 0px 8px 0px;
+          text-transform: uppercase;
+        }
+
+        h4:hover {
+          cursor: pointer;
+          text-decoration: underline;
+          transform: scale(1.025);
+        }
+      `}</style>
+    </>
+  );
+};
