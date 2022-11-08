297c476d76bf87e3b3377e2ce9b2e69b4e7a635a
moncon <monconish@protonmail.com>
2022-11-08 21:41:24 +0100

    react-bootstrap

--- components/cards/index.ts
+++ components/cards/index.ts
@@ -1,10 +1,12 @@
 
 // index of card styles for Fat Slob Plumbing
-import { ServicesCard } from "./ServicesCard";
-import { CustReviewCard } from "./CustReviewCard";
+import  {ServicesCard}  from "./ServicesCard";
+import  {CustReviewCard}  from "./CustReviewCard";
+import {FSPForm}  from "./FSPForm";
 
 
 export const Cards = {
     ServicesCard,
     CustReviewCard,
+    FSPForm,
 }
--- components/sections/FranchisesSection.tsx
+++ components/sections/FranchisesSection.tsx
@@ -1,31 +1,36 @@
 // define Franchises Section properties
-import {Typography} from "../typography";
+import { Typography } from "../typography";
+import { FSPForm } from "../cards/FSPForm";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-type FranchisesSectionProps = {
-
-};
+type FranchisesSectionProps = {};
 
 export const FranchisesSection = () => {
-    return(
-        <>
-        <div className="franchisesSection">
-            <div className="franchisesTitle">
-                <Typography.SectionTitle content="Franchises Available Now!" color="var(--solidBrown)"/>;
-            </div>
+  return (
+    <>
+      <div className="franchisesSection ps-3">
+        <div className="franchisesTitle">
+          <Typography.SectionTitle
+            content="Franchises Available Now!"
+            color="var(--solidBrown)"
+          />
         </div>
-        
-        
-        <style jsx>{`
-        .franchisesSection{
-            background-color: var(--hintOfBrown);
-            min-height: 100vh;
-            padding:40px 0px 20px 0px;
 
+        {/* <FSPForm></FSPForm> */}
+      </div>
+
+      <style jsx>{`
+        .franchisesSection {
+          background-color: var(--hintOfBrown);
+          min-height: 100vh;
+          padding: 40px 0px 20px 0px;
         }
-        .franchisesTitle{
-            padding:0px 0px 40px 0px;
+        .franchisesTitle {
+          padding: 0px 0px 40px 0px;
         }
-        `}</style>
-        </>
-    )
-}
+      `}</style>
+    </>
+  );
+};
