f5574197990de3b2928befe17b6708ac0f7993d0
MarkH <markh@bellemoncon>
2022-12-28 12:45:44 +0100

    remove BS from Franchises section

--- components/sections/FranchisesSection.tsx
+++ components/sections/FranchisesSection.tsx
@@ -1,32 +1,21 @@
 // define Franchises Section properties
 import { Typography } from "../typography";
 import { FSPForm } from "../cards/FSPForm";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
 type FranchisesSectionProps = {};
 
 export const FranchisesSection = () => {
   return (
     <>
-      <section className="franchisesSection ps-3">
+      <section className="franchisesSection">
         <div className="franchisesTitle">
           <Typography.SectionTitle
             content="Franchises Available Now!"
             color="var(--solidBrown)"
           />
         </div>
-        <Container fluid="sm">
-          <Row>
-            <Col sm={10} md="auto">
-              <FSPForm></FSPForm>
-            </Col>
-            <Col></Col>
 
-            <Col></Col>
-          </Row>
-        </Container>
+        <FSPForm></FSPForm>
       </section>
 
       <style jsx>{`
