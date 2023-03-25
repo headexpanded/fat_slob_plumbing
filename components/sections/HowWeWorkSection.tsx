24b57b26631e575a9fcfec3cf902ff8e93a227ff
MarkH <markh@bellemoncon>
2023-03-26 00:22:49 +0100

    Fixed ParaL component positions

--- components/sections/HowWeWorkSection.tsx
+++ components/sections/HowWeWorkSection.tsx
@@ -17,11 +17,15 @@ export const HowWeWorkSection = () => {
           content="Dedicated Experts"
           color="var(--defaultLight)"
         />
-        <Typography.ParaL content=" Our professional toilet testers visit your home." />
+        <Typography.ParaL
+          content=" Our professional toilet testers visit your home."
+          width=""
+        />
         <Typography.ParaL
           content="By the time we
                           leave, you'll know if your
                           toilet can take it - or not!"
+          width=""
         />
 
         <Spacer />
@@ -29,21 +33,43 @@ export const HowWeWorkSection = () => {
           content="Regular Performance"
           color="var(--defaultLight)"
         />
-        <Typography.ParaL content="Every day, we ingest a special blend of food and drink." />
-        <Typography.ParaL content="Curries. Kebabs. Lager. Full English. And so on." />
-        <Typography.ParaL content="This ensures regular performance." />
+        <Typography.ParaL
+          content="Every day, we ingest a special blend of food and drink."
+          width=""
+        />
+        <Typography.ParaL
+          content="Curries. Kebabs. Lager. Full English. And so on."
+          width=""
+        />
+        <Typography.ParaL
+          content="This ensures regular performance."
+          width=""
+        />
         <Spacer />
         <Typography.SubHeader
           content="Fast Results"
           color="var(--defaultLight)"
         />
-        <Typography.ParaL content="Each job starts with coffee, & a few cigarettes." />
-        <Typography.ParaL content="This makes sure there's no waiting around." />
-        <Typography.ParaL content="Because you can't wait for results." />
-        <Typography.ParaL content="And we can't wait to get started." />
+        <Typography.ParaL
+          content="Each job starts with coffee & a few cigarettes."
+          width=""
+        />
+        <Typography.ParaL
+          content="This makes sure there's no waiting around."
+          width=""
+        />
+        <Typography.ParaL
+          content="Because you can't wait for results."
+          width=""
+        />
+        <Typography.ParaL
+          content="And we can't wait to get started."
+          width=""
+        />
         <Typography.ParaL
           content="At your request, we bring our own reading material."
           color="var(--defaultLight)"
+          width=""
           fontWeight="900"
         />
         {/* <Typography.ParaL content="Don't wait - book us today!" />
