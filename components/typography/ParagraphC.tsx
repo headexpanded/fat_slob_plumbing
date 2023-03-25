87e896131daa52898814b47e4a4d9253238da48a
MarkH <markh@bellemoncon>
2023-03-25 15:37:02 +0100

    Added opitional color param

--- components/typography/ParagraphC.tsx
+++ components/typography/ParagraphC.tsx
@@ -3,21 +3,20 @@
 type ParagraphCProps = {
   content: string;
   color?: string;
+  fontSize?: string;
   fontWeight?: string;
 };
 
 // define Paragraph component
-export const ParagraphC = ({ content, color, fontWeight }: ParagraphCProps) => {
+export const ParagraphC = ({ content, color, fontSize, fontWeight }: ParagraphCProps) => {
   return (
     <>
       <h2>{content}</h2>
       <style jsx>{`
         h2 {
           color: ${color ?? ""};
-          font-size: 2rem;
+          font-size: ${fontSize ?? "2rem"};
           font-weight: ${fontWeight ?? "700"};
-          // text-align: center;
-          // padding: 0px 0px 4px 0px;
         }
       `}</style>
     </>
