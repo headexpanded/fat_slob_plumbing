3d9ed57e0732d4e0ce2a5cbac5bf210d3fd2502e
MarkH <markh@bellemoncon>
2023-03-25 19:42:30 +0100

    Franchise section with Trish photo

--- components/sections/FranchisesSection.tsx
+++ components/sections/FranchisesSection.tsx
@@ -1,6 +1,8 @@
 // define Franchises Section properties
+import Image from "next/image";
 import { Typography } from "../typography";
 import { Spacer } from "./Spacer";
+import trishPhoto from "../../assets/img/trishPhoto.png";
 
 type FranchisesSectionProps = {};
 
@@ -20,45 +22,117 @@ export const FranchisesSection = () => {
           color="var(--defaultLight)"
         />
         <Spacer />
-        <Typography.ParaL
-          content="Be your own boss."
-          color=""
-          fontWeight="700"
-        />
-        <Typography.ParaL
-          content="Get out and about."
-          color=""
-          fontWeight="700"
-        />
-        <Typography.ParaL
-          content="Make people happy."
-          color=""
-          fontWeight="700"
-        />
+        <div className="franchiseImageAndTextBlock">
+          <div className="trish">
+            <Image
+              alt="Photo of Fat Slob Plumbing secretary"
+              src={trishPhoto}
+              width={5184}
+              height={3456}
+              priority
+            />
+          </div>
+          <div className="franchiseText">
+            <Typography.ParaL
+              content="Be your own boss."
+              color="var(--defaultLight)"
+              fontWeight="900"
+            />
+            <Typography.ParaL
+              content="Get out and about."
+              color="var(--defaultLight)"
+              fontWeight="900"
+            />
+            <Typography.ParaL
+              content="Make people happy."
+              color="var(--defaultLight)"
+              fontWeight="900"
+            />
+            <Typography.ParaL
+              content="We're looking for people who can deliver value."
+              color="var(--defaultLight)"
+              fontWeight="900"
+            />
+            <Typography.ParaL
+              content="People who can fill a bowl."
+              color=""
+              fontWeight="900"
+            />
+            <Typography.ParaL
+              content="Any time, any place."
+              color="var(--defaultLight)"
+              fontWeight="900"
+            />
+            <Typography.ParaL
+              content="Does that sound like you?"
+              color="var(--defaultLight)"
+              fontWeight="900"
+            />
+            <Typography.ParaL
+              content="Buy a Fat Slob Plumbing franchise today!"
+              color="var(--defaultLight)"
+              fontWeight="900"
+            />
+          </div>
+        </div>
+        <Spacer />
 
-        <Typography.ParaL
-          content="We're looking for people who can deliver value."
-          color=""
-          fontWeight="700"
-        />
-
-        <Typography.ParaL
-          content="People who can fill a bowl any time, any place."
-          color=""
-          fontWeight="700"
-        />
-
-        <Typography.ParaL
-          content="If that sounds like you - yes YOU, reading this right now - then you should buy a Fat Slob Plumbing franchise!"
-          color="var(--defaultLight)"
-          fontWeight="700"
-        />
+        <div className="phoneTrish">
+          <Typography.SubHeader
+            content="Trish is waiting for your call!"
+            color="var(--defaultLight)"
+          />
+        </div>
+        <Spacer />
       </section>
 
       <style jsx>{`
         .franchisesSection {
         }
         .franchisesTitle {
+        }
+
+        .franchiseImageAndTextBlock {
+          display: flex;
+          flex-direction: column;
+          place-items: center;
+        }
+
+        .trish,
+        .phoneTrish {
+          display: none;
+        }
+
+        .franchiseText {
+          padding-left: 20px;
+          max-width: 90%;
+        }
+
+        @media screen and (min-width: 640px) {
+          .franchiseImageAndTextBlock {
+            flex-direction: row;
+            justify-content: space-between;
+          }
+          .trish {
+            display: flex;
+            width: 50%;
+            height: auto;
+          }
+          .phoneTrish {
+            display: flex;
+            width: clamp(240px, 60vw + 36px, 600px);
+          }
+          .franchiseText {
+            width: 50%;
+          }
+        }
+
+        @media screen and (min-width: 1028px) {
+          .franchiseText {
+          }
+          .trish {
+            overflow: hidden;
+          }
         }
       `}</style>
     </>
