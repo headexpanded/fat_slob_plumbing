bcda32bf0623940da66bd8dba0292b8ca253a1f7
MarkH <markh@bellemoncon>
2023-03-25 15:36:22 +0100

    Footer section style

--- components/sections/FooterSection.tsx
+++ components/sections/FooterSection.tsx
@@ -10,16 +10,27 @@ export const FooterSection = () => {
   return (
     <>
       <footer className="footerSection ">
-        <p>
-          We are not licenced plumbers. In case of emergency, you should contact
-          a real plumber.
-        </p>
+        <Typography.ParaC
+          color="var(--defaultDark)"
+          content="We are not licenced plumbers."
+          fontSize="1rem"
+          fontWeight="500"
+        />
+        <Typography.ParaC
+          color="var(--defaultDark)"
+          content="In case of emergency, you should contact
+          a real plumber."
+          fontSize="1rem"
+          fontWeight="500"
+        />
       </footer>
       <style jsx>{`
         .footerSection {
-          // background-color: var(--PVgold);
-          display:flex;
-          justify-content: center;
+          background-color: var(--defaultLight);
+          display: flex;
+          flex-direction: column;
+          padding: 0.5rem 0px;
+          place-items: center;
         }
         .footerSection p {
           color: var(--defaultDark);
