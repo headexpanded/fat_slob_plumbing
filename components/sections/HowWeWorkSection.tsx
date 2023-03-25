afb2db36e4b7ec9e67e29c3173aa5aa0e3e7cd8d
MarkH <markh@bellemoncon>
2023-03-25 14:47:49 +0100

    Edited copy

--- components/sections/HowWeWorkSection.tsx
+++ components/sections/HowWeWorkSection.tsx
@@ -13,54 +13,44 @@ export const HowWeWorkSection = () => {
           content="How We Work"
           color="var(--defaultDark)"
         />
-
+        <Typography.SubHeader
+          content="Dedicated Experts"
+          color="var(--defaultLight)"
+        />
         <Typography.ParaL
           content=" Our experienced,
                     professional toilet testers visit your home."
-          color=""
-          fontWeight="700"
         />
-
         <Typography.ParaL
           content="By the time we
                           leave, you'll know if your
                           toilet can take it - or not!"
         />
+
         <Spacer />
         <Typography.SubHeader
-          content="Dedicated Experts"
+          content="Regular Performance"
           color="var(--defaultLight)"
         />
-        <Typography.ParaL content="Every day, your Fat Slob Plumber ingests a carefully formulated blend of food and drink - curries, kebabs, lager, full English, and so on." />
-
-        <Typography.ParaL
-          content="This ensures optimal performance."
-          color=""
-        />
+        <Typography.ParaL content="Every day, we ingest a special blend of food and drink." />
+        <Typography.ParaL content="Curries. Kebabs. Lager. Full English. And so on." />
+        <Typography.ParaL content="This ensures regular performance." />
         <Spacer />
-        <Typography.SubHeader
-          content="Attention To Detail"
-          color="var(--defaultLight)"
-        />
-        <Typography.ParaL
-          content=" 
-                    We also consume a mix of supplementary nutriments when we're at your home - like coffee, cigarettes, & Coke - to make sure there's no waiting around."
-          color=""
-          fontWeight="700"
-        />
-
+        <Typography.SubHeader content="100% Commitment" color="var(--defaultLight)" />
+        <Typography.ParaL content="Every morning starts with coffee, & a few cigarettes." />
+        <Typography.ParaL content="This makes sure there's no waiting around." />
+        <Typography.ParaL content="Because you can't wait for results." />
+        <Typography.ParaL content="And we can't wait to get started." />
         <Typography.ParaL
           content="At your request, we bring our own reading material."
           color="var(--defaultLight)"
           fontWeight="700"
         />
-
         <Typography.ParaL
           content="Don't wait - book us today!"
           color=""
           fontWeight="700"
         />
-
         <Typography.ParaL
           content="Or sign up for a monthly plan."
           color=""
