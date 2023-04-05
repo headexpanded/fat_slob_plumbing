c47c9beebab367e6122ace292dc282be991281b6
MarkH <markh@bellemoncon>
2023-04-05 11:01:02 +0200

    added Vercel Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,14 @@
 import "../styles/globals.css";
 import { useEffect } from "react";
 import type { AppProps } from "next/app";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return(
+  <>
+    <Component {...pageProps} />
+    <Analytics />
+  </>);
 }
 
 export default MyApp;
