4d57aa152121792e5e63024a1e16b7ae9e02e41b
moncon <markh@bellemoncon>
2023-06-29 17:10:24 +0200

    Our Story text edit

--- components/sections/OurStorySection.tsx
+++ components/sections/OurStorySection.tsx
@@ -16,220 +16,105 @@ export const OurStorySection = () => {
         </div>
 
         <Typography.ParaL
-          content="It was a Saturday morning like any other..."
+          content="This is not actually a real business..."
           color="var(--defaultFontColor)"
         />
 
         <Typography.ParaL
-          content="Ron woke up. Made a pot of tea. Flicked on the radio."
+          content="It's just a site I made for my teenage son and his friends."
           color="var(--defaultFontColor)"
         />
 
         <Typography.ParaL
-          content="And sat down on the toilet."
-          color="var(--defaultFontColor)"
-        />
-
-        <Typography.ParaL
-          content="He wasn't expecting much."
-          color="var(--defaultFontColor)"
-        />
-
-        <Typography.ParaL
-          content="After all, it hadn't been a big night, just the seven pints and two kebabs."
-          color="var(--defaultFontColor)"
-        />
-        <Spacer />
-
-        <Typography.ParaC
-          content="Horror in the Morning"
-          color="var(--defaultLight)"
-          fontSize="3rem"
-          fontWeight="900"
-        />
-
-        <Typography.ParaL
-          content="But ten minutes later, Ron got the surprise of his life."
-          color="var(--defaultFontColor)"
-        />
-
-        <Typography.ParaL
-          content="The toilet was blocked!"
-          color="var(--defaultFontColor)"
-        />
-
-        <Typography.ParaL
-          content="The stupid thing just wouldn't flush it away, no matter how much he pogo'd the scrubbing brush."
+          content="And yes, they think it's funny."
           color="var(--defaultFontColor)"
         />
 
         <Spacer />
 
         <Typography.ParaC
-          content="Sheer Panic"
+          content="Pure CSS"
+          color="var(--defaultLight)"
+          fontSize="3rem"
+          fontWeight="900"
+        />
+
+        <Spacer />
+
+        <Typography.ParaL
+          content="For the styling, I had three main objectives:"
+          color="var(--defaultFontColor)"
+        />
+
+        <Typography.ParaC
+          content="No Framework"
           color="var(--defaultLight)"
           fontSize="3rem"
           fontWeight="900"
         />
 
         <Typography.ParaL
-          content="Ron tried everything. Boiling water. A litre of bleach. Paint thinner."
+          content="First, to write pure CSS without using a framework such as Tailwind or Bootstrap."
           color="var(--defaultFontColor)"
         />
 
         <Typography.ParaL
-          content="But nothing worked. And time was ticking by!"
+          content="I wanted to see how much I could do writing the CSS myself (that's why there are still a few niggly little things to fix up)."
+          color="var(--defaultFontColor)"
+        />
+
+        <Typography.ParaC
+          content="Cascading Styles"
+          color="var(--defaultLight)"
+          fontSize="3rem"
+          fontWeight="900"
+        />
+
+        <Typography.ParaL
+          content="Secondly, to make the CSS cascade down through the HTML elements and match the semantics."
           color="var(--defaultFontColor)"
         />
 
         <Typography.ParaL
-          content="Ron would be late for warm-up with the darts team at The King's Seat!"
+          content="For example, set default styles for the html <section> element, then override as necessary."
           color="var(--defaultFontColor)"
         />
 
         <Spacer />
 
         <Typography.ParaC
-          content="Disaster. Shame. Ruin."
+          content="Custom Functions"
           color="var(--defaultLight)"
           fontSize="3rem"
           fontWeight="900"
         />
-        <Typography.ParaL
-          content="Two hours later, Ron finally got the toilet unblocked."
-          color="var(--defaultFontColor)"
-        />
 
         <Typography.ParaL
-          content="Don't ask him how. The memories are too painful."
-          color="var(--defaultFontColor)"
-        />
-
-        <Typography.ParaL
-          content="He'd missed opening time."
-          color="var(--defaultFontColor)"
-        />
-
-        <Typography.ParaL
-          content="He'd missed the darts warm-up."
-          color="var(--defaultFontColor)"
-        />
-
-        <Typography.ParaL
-          content="And he'd missed his usual full English."
+          content="Thirdly, to use custom functions like min(), max(), calc(), and clamp() instead of media queries where it made sense to do so."
           color="var(--defaultFontColor)"
         />
 
         <Spacer />
 
         <Typography.ParaC
-          content="Hopeless Despair"
+          content="More To Do"
           color="var(--defaultLight)"
           fontSize="3rem"
           fontWeight="900"
         />
 
         <Typography.ParaL
-          content="Ron raised his eyes to the ceiling and let out an anguished cry of despair!"
+          content="I'd like to use CSS modules instead of a single global style sheet, but for this site I'm not sure it's worth it"
           color="var(--defaultFontColor)"
         />
 
         <Typography.ParaL
-          content="Total despair!"
-          color="var(--defaultFontColor)"
-        />
-
-        <Typography.ParaL
-          content="Not just for himself."
-          color="var(--defaultFontColor)"
-        />
-
-        <Typography.ParaL
-          content="Not just for the darts team."
-          color="var(--defaultFontColor)"
-        />
-
-        <Typography.ParaL
-          content="But for the whole of humanity!"
-          color="var(--defaultFontColor)"
-        />
-
-        <Typography.ParaL
-          content="HOW CAN WE GO ON LIVING LIKE THIS?!?!"
+          content="Also, CSS layers would be fun to learn, so maybe I'll do that sooner or later."
           color="var(--defaultFontColor)"
         />
 
         <Spacer />
 
-        <Typography.ParaC
-          content="A Light From Above."
-          color="var(--defaultLight)"
-          fontSize="3rem"
-          fontWeight="900"
-        />
-
-        <Typography.ParaL
-          content="And it was then - in the most profound depths of his hopeless melancholy - that Ron had one of them epiphany things."
-          color="var(--defaultFontColor)"
-        />
-
-        <Typography.ParaL
-          content="You know, like in the old days."
-          color="var(--defaultFontColor)"
-        />
-
-        <Typography.ParaL
-          content="For Ron suddenly realised that he was not alone. Not alone!"
-          color="var(--defaultFontColor)"
-        />
-
-        <Typography.ParaL
-          content="Ron realised that thousands - millions! - of other people must also fear the terrible risk of an unexpected toilet blockage."
-          color="var(--defaultFontColor)"
-        />
-
-        <Typography.ParaL
-          content="And Ron knew that he could help."
-          color="var(--defaultFontColor)"
-        />
-
-        <Spacer />
-
-        <Typography.ParaC
-          content="Saving Humanity"
-          color="var(--defaultLight)"
-          fontSize="3rem"
-          fontWeight="900"
-        />
-
-        <Typography.ParaL
-          content="In that moment, Ron decided to devote his life to saving humanity from the horror of a blocked toilet."
-          color="var(--defaultFontColor)"
-        />
-
-        <Typography.ParaL
-          content="Quickly Ron phoned his mate Dave."
-          color="var(--defaultFontColor)"
-        />
-
-        <Typography.ParaL
-          content="'Dave!', he said, 'Dave! I've got an idea!'"
-          color="var(--defaultFontColor)"
-        />
-
-        <Typography.ParaL
-          content="Fat Slob Plumbing was born."
-          color="var(--defaultFontColor)"
-          fontWeight="700"
-        />
-
-        <Typography.ParaL
-          content="The rest is history."
-          color="var(--defaultFontColor)"
-          fontWeight="700"
-        />
-
-        <Spacer />
       </section>
 
       <style jsx>{`
