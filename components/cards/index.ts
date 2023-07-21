c8ed8ffbb59f2876ecc2214ff0052096eb452760
moncon <markh@bellemoncon>
2023-07-21 18:08:59 +0200

    CARDS: update cards/index.ts
    - removes deleted card imports

--- components/cards/index.ts
+++ components/cards/index.ts
@@ -1,12 +1,9 @@
 // index of card styles for Fat Slob Plumbing
-import { ServicesCard } from "./ServicesCard";
+
 import { CustReviewCard } from "./CustReviewCard";
 import { PartnerCard } from "./PartnerCard";
-import { FSPForm } from "./FSPForm";
 
 export const Cards = {
-  ServicesCard,
   CustReviewCard,
   PartnerCard,
-  FSPForm,
 };
