132acb33273f392b62319a1b36fa93660cffbcf7
moncon <markh@bellemoncon>
2023-08-06 15:19:34 +0200

    STRUCT: _document.tsx from class to function
    - changes _document.tsx from a class to a functional component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,5 +10,21 @@ export default function Document() {
     </body>
     </Html>
   );
+<<<<<<< HEAD
+=======
+  /* static async getInitialProps(ctx: DocumentContext) {
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () => originalRenderPage();
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+      };
+    } finally {
+      ctx.renderPage();
+    }
+  } */
+>>>>>>> 4a25756 (STRUCT: _document.tsx from class to function)
 }
 
